2982f531d2e8ed8a9935a3fab46998707bd0036e
Max <maxigarcette@gmail.com>
2023-10-16 20:07:31 -0300

    entrega

---
+++ ProductManager.js
@@ -0,0 +1,42 @@
+
+class ProductManager{
+    
+    static #precioBaseGanancia=0.15
+
+    constructor(){
+        this.productos=[]
+    }
+
+    getProducts(){
+        return this.productos
+    }
+
+    addProducts(producto, descripcion, img, precio, codigo, stock){
+
+        let id=1
+        if(this.productos.length>0){
+            id=this.productos[this.productos.length-1].id + 1
+        }
+        
+        let nuevoProducto ={
+            id,
+            producto, descripcion,
+            precio: precio + precio*ProductManager.#precioBaseGanancia,
+            img, codigo, stock
+        }
+
+        this.productos.push(nuevoProducto)
+    }
+
+    getProductById(id){
+        let indice = this.productos.findIndex(producto=>producto.id===id)
+        if(indice===-1){
+            console.log("not found")
+            return
+        }
+        return this.productos[indice]
+    }
+}
+
+let ProductManager = new ProductManager()
+ProductManager.addProducts
